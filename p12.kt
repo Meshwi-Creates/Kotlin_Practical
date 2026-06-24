95a67c186d4546ea5b70f647651d6610733af56a
Meshwi-Creates <meshwi.creates@gmail.com>
2026-06-24 12:23:00 +0530

    Add files via upload

---
+++ p12.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val i=20000
+    var m="3245"
+    val j:Double = i.toDouble()
+    val k:Int = m.toInt()
+    val n:Double = m.toDouble()
+
+    println("integer value=$i")
+    println("double value (from integer)=$j")
+    println("string value=$m")
+    println("integer value (from string)=$k")
+    println("double value (from string)=$n")
+
+}
